8087d1def60c826df4c7eaf856816a299225419b
jalal (Tim Pushman) <thpushman@gmail.com>
2014-06-15 17:58:34 +0100

    added comments

--- WQFlickr/Photo.php
+++ WQFlickr/Photo.php
@@ -1,10 +1,14 @@
 <?php
 /**
- *
- *
- * copyright: 2013 jalal @ gnomedia
- * license: MIT License, see LICENSE file
- */
+  * THe photo class
+  *
+  *  @category  API
+  *  @package   Flickr
+  *  @author    jalal @ gnomedia <jalal@gnomedia.com>
+  *  @copyright 2013 jalal @ gnomedia
+  *  @license   MIT License, see LICENSE file
+  *  @link
+  */
 
 namespace WQFlickr;
 
@@ -21,31 +25,57 @@ class Photo
   private $isprimary;
   private $basic_url = 'http://farm%d.staticflickr.com/%s/%s_%s_%s.jpg';
 
+  /**
+   * [__construct description]
+   *
+   * @param [type] $data [description]
+   */
   public function __construct($data)
   {
-    $this->id = $data['id'];
-    $this->secret = $data['secret'];
-    $this->server = $data['server'];
-    $this->farm = $data['farm'];
-    $this->title = $data['title'];
+    $this->id        = $data['id'];
+    $this->secret    = $data['secret'];
+    $this->server    = $data['server'];
+    $this->farm      = $data['farm'];
+    $this->title     = $data['title'];
     $this->isprimary = $data['isprimary'];
   }
 
+  /**
+   * [setSrc description]
+   *
+   * @param string $value [description]
+   */
   public function setSrc($value='')
   {
     $this->src = $value;
   }
 
+  /**
+   * [setOrig description]
+   *
+   * @param string $value [description]
+   */
   public function setOrig($value='')
   {
     $this->orig = $value;
   }
 
+  /**
+   * [getSrc description]
+   *
+   * @param  string $size [description]
+   * @return [type]       [description]
+   */
   public function getSrc($size='t')
   {
     return sprintf($this->basic_url, $this->farm, $this->server, $this->id, $this->secret, $size);
   }
 
+  /**
+   * [getTitle description]
+   *
+   * @return [type] [description]
+   */
   public function getTitle()
   {
     return $this->title;
